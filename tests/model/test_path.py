3c0b0bb6594dcde98efb8c3528c4bcfbd5165e0b
Eduardo Mezêncio <eduardomezencio@protonmail.com>
2019-11-02 19:53:15 -0300

    Create unit test for drop_duplicates and fix dijkstra test.

--- tests/model/test_path.py
+++ tests/model/test_path.py
@@ -6,7 +6,7 @@ from tsim.model.geometry import Point
 from tsim.model.index import INSTANCE as INDEX
 from tsim.model.network.node import Node
 from tsim.model.network.path import dijkstra
-from tsim.model.network.way import Way
+from tsim.model.network.way import Endpoint, Way
 
 INDEX.name = 'test'
 
@@ -18,11 +18,11 @@ class PathTest(unittest.TestCase):
         """Test the dijkstra algorithm implementation.
 
         Generate a size x size grid of ways and find use dijkstra on the first
-        node. Check that the first node points to None and the others all reach
-        the first node with a valid path.
+        oriented way. Check that the first oriented way points to None and the
+        others all reach the first node with a valid path.
         """
         size = 3
-        position = [p for p in product(range(0, 100 * size, 100), repeat=2)]
+        position = list(product(range(0, 100 * size, 100), repeat=2))
         nodes = [Node(Point(*p)) for p in position]
         ways = []
         for i, node in enumerate(nodes):
@@ -30,12 +30,18 @@ class PathTest(unittest.TestCase):
                 ways.append(Way(node, nodes[i + 1], (1, 1)))
             if i < len(nodes) - size:
                 ways.append(Way(node, nodes[i + size], (1, 1)))
-        paths = dijkstra(nodes[0])
-        for node, way in paths.items():
-            node_, way_ = node, way
-            while node_ is not nodes[0]:
-                self.assertIs(node_, way_.end)
-                node_ = way_.start
-                way_ = paths[node_]
+        oriented_ways = [w.oriented() for w in ways]
+        oriented_ways.extend(w.oriented(Endpoint.END) for w in ways
+                             if w.lane_count[1] > 0)
+        single_source = dijkstra(oriented_ways[0])
+        for way, (_, depth) in single_source.items():
+            depth += 1
+            way_ = way
+            while way_ is not oriented_ways[0]:
+                way_, depth_ = single_source[way_]
+                self.assertEqual(depth_, depth - 1)
+                depth = depth_
+            self.assertEqual(depth, 1)
+            way_, depth_ = single_source[way_]
+            self.assertEqual(depth_, 0)
             self.assertIsNone(way_)
-        self.assertTrue(paths)
--- tests/utils/test_iterators.py
+++ tests/utils/test_iterators.py
@@ -2,11 +2,20 @@
 import unittest
 from itertools import islice
 
-from tsim.utils.iterators import window_iter
+from tsim.utils.iterators import drop_duplicates, window_iter
 
 
 class IteratorsTest(unittest.TestCase):
     """Unit test for iterators."""
+
+    def test_drop_duplicates(self):
+        """Unit test for drop_duplicates."""
+        list_ = [1, 2, 3, 4, 5]
+        self.assertEqual(list(drop_duplicates(list_)), list_)
+
+        list_ = [1, 2, 3, 2]
+        self.assertEqual(list(drop_duplicates([1, 2, 2, 3, 2])), list_)
+        self.assertEqual(list(drop_duplicates([1, 1, 2, 3, 3, 3, 2])), list_)
 
     def test_window_iter(self):
         """Unit test for window_iter."""
